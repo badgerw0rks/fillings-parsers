9d299b2d8088933d05d8c983aa5432d2dfacaada
Shakir <shakirali2244@yahoo.com>
2025-04-07 01:17:00 -0700

    fetch data from sec server

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const SEC = "https://data.sec.gov/api/xbrl/companyfacts/"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,10 +13,22 @@ router.get('/', function(req, res, next) {
 router.get('/ren/:cik', function(req, res, next) {
   
 
-  let rawdata = fs.readFileSync('../../DATA/companyfacts/'+req.params.cik);
-  let xbrl = JSON.parse(rawdata);
+  //let rawdata = fs.readFileSync('../../DATA/companyfacts/'+req.params.cik);
 
-  res.render('ren', {name: xbrl["facts"]} );
+const uri = SEC + req.params.cik
+
+console.log('calling ' + uri)
+
+fetch(uri)
+  .then((response) => response.text())
+  .then((body) => {
+    
+let xbrl = JSON.parse(body);
+
+res.render('ren', {name: xbrl["facts"]} );
+      //console.log(body);
+});
+
 });
 
 
